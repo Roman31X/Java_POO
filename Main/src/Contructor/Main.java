325c1b3416ad49a87dfa7378501b545eeee94894
Roman31X <roman.haa31@gmail.com>
2023-04-19 19:37:05 -0500

    Java_POO_Dia

---
+++ Main/src/Contructor/Main.java
@@ -0,0 +1,7 @@
+package Contructor;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Main/src/Modulos_Clases/Main.java
+++ Main/src/Modulos_Clases/Main.java
@@ -1,4 +1,17 @@
 package Modulos_Clases;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        //implementar la libreria Scanner
+        Scanner sc = new Scanner (System.in);
+        //creamos un objeto
+        /**
+         * Declaramos primero al módulo [Doctor] para
+         * después declara el objeto [doc]
+         * */
+        Doctor doc = new Doctor();
+        //le dare valores a las variables de la clase
+        doc.nombre="Diego Roman";
+        doc.Nombre();
+    }
 }

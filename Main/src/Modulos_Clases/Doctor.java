ff95cd71375e45c6bf622a5c0af21a2515275b1a
Roman31X <roman.haa31@gmail.com>
2023-04-19 19:15:31 -0500

    Java_POO_Dia1

--- Main/src/Modulos_Clases/Doctor.java
+++ Main/src/Modulos_Clases/Doctor.java
@@ -1,2 +1,13 @@
-package Modulos_Clases;public class Doctor {
+package Modulos_Clases;
+
+public class Doctor {
+    //Declaramos variables
+    int id;
+    String nombre;
+    String especialidad;
+    //esta clase solo imprimira la clase
+    public void Nombre(){
+        System.out.println(nombre);
+    }
+
 }

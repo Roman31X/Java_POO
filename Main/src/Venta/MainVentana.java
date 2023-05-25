64ae5d17c2941e6a684c710dd9bd4af3c61d7e83
Roman31X <die.haa65@gmail.com>
2023-05-25 18:35:17 -0500

    Curso_Java_POO_Dia42.4

---
+++ Main/src/Venta/MainVentana.java
@@ -0,0 +1,11 @@
+package Venta;
+
+public class MainVentana {
+    public static void main(String[] args) {
+        //creamos un objeto de la clase ventana
+        Venta ven = new Venta();
+        //para hacer visible la ventana
+        ven.setVisible(true);
+    }
+
+}
--- Main/src/Venta/Venta.java
+++ Main/src/Venta/Venta.java
@@ -1,4 +1,21 @@
 package Venta;
 
-public class Venta {
+import javax.swing.*;
+
+//PARA UTILIZAR EL PAQUETE DE GRAFICA EN MI CLASE
+//SE DEBE DE IMPORTAR "import javax.swing.*;"
+//Y EN LE MÉTODO DE LA CLASE AGREGAR EL "extends JFrame"
+//MAS INFORMACIÓN => "https://docs.oracle.com/en/java/javase/17/docs/api/java.desktop/javax/swing/JFrame.html"
+public class Venta extends JFrame {
+    //CONSTRUCTOR DE LA CLASE
+    public Venta(){
+        //DAMOS TAMAÑO A LA VENTA A TRAVES DE LA SIGUIENTE DECLARACIÓN
+        //mediante la sentencia le pasaremos los argumentos
+        //COMO PRIMER ARGUMENTO EL [ANCHO] de la ventana
+        //COMO SEGUNDO ARGUMENTO EL [LARGO] de la ventana
+        setSize(500,400);
+        //designamos un nombre a la ventana grafica
+        setTitle("Mi primera Ventana Jframe");
+
+    }
 }

e238c68ca1e9b9bf0d1a823801d938f7b9792fb3
Roman31X <die.haa65@gmail.com>
2023-05-25 20:09:12 -0500

    Curso_Java_POO_Dia43.1

--- Main/src/Venta/MainVentana.java
+++ Main/src/Venta/MainVentana.java
@@ -1,11 +1,14 @@
 package Venta;
 
+import javax.swing.*;
+
 public class MainVentana {
     public static void main(String[] args) {
         //creamos un objeto de la clase ventana
         Venta ven = new Venta();
         //para hacer visible la ventana
         ven.setVisible(true);
+
     }
 
 }
--- Main/src/Venta/Venta.java
+++ Main/src/Venta/Venta.java
@@ -1,6 +1,7 @@
 package Venta;
 
 import javax.swing.*;
+import java.awt.*;
 
 //PARA UTILIZAR EL PAQUETE DE GRAFICA EN MI CLASE
 //SE DEBE DE IMPORTAR "import javax.swing.*;"
@@ -12,10 +13,47 @@ public class Venta extends JFrame {
         //DAMOS TAMAÑO A LA VENTA A TRAVES DE LA SIGUIENTE DECLARACIÓN
         //mediante la sentencia le pasaremos los argumentos
         //COMO PRIMER ARGUMENTO EL [ANCHO] de la ventana
-        //COMO SEGUNDO ARGUMENTO EL [LARGO] de la ventana
+        //COMO SEGUNDO ARGUMENTO EL [LARGO de la ventana
         setSize(500,400);
         //designamos un nombre a la ventana grafica
         setTitle("Mi primera Ventana Jframe");
-
+        //SENTENCIA PARA DETENER LA EJECUCIÓN DESDE LA VENTANA
+        //por defecto tomara la función cerrar la barra donde esta
+        //el titulo
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //establecemos la posicion de la ventana en el plano
+        //[x] y [y]
+        /**setLocation(100,200);*/
+        //para reducir lines d codigo en una sola sentencia se puede
+        //indicar la posición y el tamaño de la ventana
+        /**Los dos primeros argumentos determinan la posición*/
+        /**Los dos segundos argumentos determinan el tamaño de la ventana*/
+        //setBounds(100,200,500,400);
+        //para centrar la ventana
+        setLocationRelativeTo(null);
+        //establecer color a la Ventana
+        //this.getContentPane().setBackground(Color.CYAN);
+        //lamamos al metodo que contiene al panel
+        iniciarComponentes();
+    }
+    //agregar un panel a nuestra ventana
+    private void iniciarComponentes(){
+        JPanel panel1 = new JPanel(); //creación de un panel
+        //colocar encima de la Ventana
+        this.getContentPane().add(panel1);
+        //para darle color al panel
+        //panel1.setBackground(Color.CYAN);
+        //Para desactivar el Layout o Diseño por defecto
+        panel1.setLayout(null);
+        //CREAMOS UNA INSTANCIA del tipo [JLabel = etiquetas]
+        //PODEMOS ENVIAR EL ARGUMENTO DE TIPO CADENA "Hola"
+        //El constructor por determinación lo puede aceptar
+        JLabel etiqueta = new JLabel();
+        //para darle nombre la etiqueta
+        etiqueta.setText("Hola");
+        //le damos tamaño y posicion a la etiqueta
+        etiqueta.setBounds(20,10,50,30);
+        //agregamos la etiqueta al panel
+        panel1.add(etiqueta);
     }
 }

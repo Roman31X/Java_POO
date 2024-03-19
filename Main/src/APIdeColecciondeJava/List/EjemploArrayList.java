c43b673970051a30b401cecf4fe505d575fb39ad
Roman31X <die.haa65@gmail.com>
2024-03-19 16:48:25 -0500

    API de LinkedList

--- Main/src/APIdeColecciondeJava/List/EjemploArrayList.java
+++ Main/src/APIdeColecciondeJava/List/EjemploArrayList.java
@@ -13,7 +13,23 @@ public class EjemploArrayList {
         al.add(new Alumno("Luci",4));
         al.add(new Alumno("Mario",7));
         al.add(new Alumno("Andres",3));
+        //AGREGAR CON INDICE EN LA LISTA
+        al.add(0,new Alumno("Soriano",7));
+        al.add(2,new Alumno("Alex",3));
         System.out.println(al + " - size : "+al.size());
 
+        //REMOVER POR INSTANCIA IQUALS
+        al.remove(new Alumno("Jano",7));
+        //al.remove(0); //REMOVER POR INDICE
+
+        //Verificar si un elemento está contenido dentro de la lista
+        boolean b = al.contains(new Alumno("Cata",6));
+        System.out.println("La lista contienes a Cata : "+b);
+
+        //Convertir un list en Arrays
+        Object a[] = al.toArray();
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Contenido en arreglo : "+a[i]);
+        }
     }
 }
---
+++ Main/src/APIdeColecciondeJava/List/EjemploLinkdList.java
@@ -0,0 +1,49 @@
+package APIdeColecciondeJava.List;
+
+import APIdeColecciondeJava.Modelo.Alumno;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjemploLinkdList {
+    public static void main(String[] args) {
+        LinkedList<Alumno> enlazada = new LinkedList<>();
+        System.out.println(enlazada + " - size : "+enlazada.size());
+        System.out.println("Esta vacía : "+enlazada.isEmpty());
+
+        enlazada.add(new Alumno("Pato",5));
+        enlazada.add(new Alumno("Cata",6));
+        enlazada.add(new Alumno("Luci",4));
+        enlazada.add(new Alumno("Mario",7));
+        enlazada.add(new Alumno("Andres",3));
+        System.out.println(enlazada + " - size : "+enlazada.size());
+
+        enlazada.addFirst(new Alumno("Diego",18)); //Agregar primero
+        enlazada.addLast(new Alumno("Dante",19)); //Agregar ultimo
+        System.out.println(enlazada + " - size : "+enlazada.size());
+
+        //Visualizar primer y último contenido de la lista y por índice
+        System.out.println("Primero en la lista : "+enlazada.getFirst());
+        System.out.println("Ultimo en la lista : "+enlazada.getLast());
+        System.out.println("Por indice en la lista 2 : "+enlazada.get(2));
+
+        //Remover primero y último
+        enlazada.removeFirst();
+        enlazada.removeLast();
+        System.out.println(enlazada + " - size : "+enlazada.size());
+
+        //Eliminar por indice
+        enlazada.remove(2);
+        System.out.println(enlazada + " - size : "+enlazada.size());
+
+        //Agregar elemento por objeto
+        Alumno a = new Alumno("Lucas",5);
+        enlazada.addLast(a);
+        System.out.println("Indice de Lucas : "+enlazada.indexOf(a));
+
+        //MODIFICAR por indice
+        enlazada.set(3,new Alumno("Lala",7));
+        System.out.println(enlazada);
+    }
+}

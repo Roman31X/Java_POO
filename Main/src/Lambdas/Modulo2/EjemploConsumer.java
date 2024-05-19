fa45450d98142044fd955052a8af6c15a1d75a9c
Roman31X <die.haa65@gmail.com>
2024-05-19 15:20:00 -0500

    Lambda Consumer

---
+++ Main/src/Lambdas/Modulo2/EjemploConsumer.java
@@ -0,0 +1,55 @@
+package Lambdas.Modulo2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class EjemploConsumer {
+    public static void main(String[] args) {
+
+        /**
+         * Esta expresión lambda nos permite pasar una expresión lambda
+         * del tipo genérico.
+         * Cuando la función únicamente tiene un proceso o función
+         * se puede omitir las llaves, pero por buenas prácticas
+         * es necesario establecer su contenido
+         * También al ser únicamente un solo argumento se puede
+         * colocar entre dos paréntesis, pero por buenas prácticas
+         * es recomendable para identificarlo
+         * Asi mismo, también se puede mencionar el tipo de argumento,
+         * pero ya que lo estamos declarando de tipo genérico String
+         * no es necesario*/
+
+        /*ejemplo de tipo String*/
+        Consumer<String> consumidor = saludo -> {
+            System.out.println(saludo);
+        };
+
+        /*Ejemplo de tipo Date*/
+        Consumer<Date> consumidor2 = (Date fecha) -> {
+            SimpleDateFormat datos = new SimpleDateFormat("yyyy-MM-dd");
+            System.out.println(fecha);
+        };
+
+        /**Para la implementación de una funciona lambda se debe
+         * de hacer el llamado mediante su objeto consumidor*/
+
+        consumidor.accept("Hola este es mi saludo por lambda");
+
+        consumidor2.accept(new Date());
+
+        /**Para la implementación para más de dos tipos de atributos
+         * por la expresión usaremos la clase BiConsumer*/
+
+        BiConsumer<String, Integer> consumidor3 = (nombre, edad) -> {
+            System.out.println("Mi nombre es: "+nombre);
+            System.out.println("Mi edad es: "+edad);
+        };
+
+        consumidor3.accept("Diego",24);
+
+
+
+    }
+}

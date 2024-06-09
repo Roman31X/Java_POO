fed5588931d24916829c85f066d0768f0565f533
Roman31X <die.haa65@gmail.com>
2024-06-09 15:00:00 -0500

    Agregadmos actionListener

--- Main/src/main/java/ProgramacionJFrame/Modulo_2_SwingIntellijIDEA/Forma.java
+++ Main/src/main/java/ProgramacionJFrame/Modulo_2_SwingIntellijIDEA/Forma.java
@@ -1,23 +1,31 @@
 package ProgramacionJFrame.Modulo_2_SwingIntellijIDEA;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Forma extends JFrame{
-    private JPanel panel1;
+    private JPanel panelPrincipal;
+    private JTextField campoTexto;
+    private JLabel replicadorLabel;
 
     public Forma(){
         inicializarForma();
+        campoTexto.addActionListener(e -> {
+            System.out.println("Se ejecuto accionLisener");
+        });
     }
 
     private void inicializarForma(){
-        setContentPane(panel1);   //para establecer el primer componente del formulario
+        setContentPane(panelPrincipal);   //para establecer el primer componente del formulario
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //esta línea nos indica que cuando cerramos la ventana terminaremos la aplicación
-        setSize(400,200); //para indicar el tamaño de nuestra ventana en pixeles alto y ancho
+        setSize(600,400); //para indicar el tamaño de nuestra ventana en pixeles alto y ancho
         setLocationRelativeTo(null); // para indicar la posición donde se mostrara nuestra ventana con ello centraremos la ventana
     }
 
     public static void main(String[] args) {
+        FlatDarculaLaf.setup();
         Forma forma = new Forma();
         forma.setVisible(true);
     }

2fd82538780ab36d07e2213b7ead54ee470087dc
Roman31X <die.haa65@gmail.com>
2024-06-09 15:10:00 -0500

    Login vista agregado

---
+++ Main/src/main/java/ProgramacionJFrame/Modulo_2_SwingIntellijIDEA/LoginForm.java
@@ -0,0 +1,10 @@
+package ProgramacionJFrame.Modulo_2_SwingIntellijIDEA;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel PanelPrincipal;
+    private JTextField usuarioTexto;
+    private JPasswordField passwordTexto;
+    private JButton enviarBoton;
+}
